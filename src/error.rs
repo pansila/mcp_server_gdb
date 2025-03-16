0c515f3d13bb8aa685f044ea7c96248484321e20
Lix Zhou <xeontz@gmail.com>
2025-03-16 20:52:19 +0800

    fix: fix CI errors
    fix them
    
    Signed-off-by: Lix Zhou <xeontz@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,3 @@
-use axum::{
-    Json,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-};
-use serde_json::json;
 use thiserror::Error;
 
 /// Application error types
@@ -13,6 +7,7 @@ pub enum AppError {
     GDBError(String),
 
     #[error("Parse error: {0}")]
+    #[allow(dead_code)]
     ParseError(String),
 
     #[error("IO error: {0}")]
@@ -20,30 +15,6 @@ pub enum AppError {
 
     #[error("Resource not found: {0}")]
     NotFound(String),
-
-    #[error("Internal server error: {0}")]
-    InternalServerError(String),
-}
-
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        let (status, error_message) = match self {
-            AppError::GDBError(msg) => (StatusCode::BAD_REQUEST, msg),
-            AppError::ParseError(msg) => (StatusCode::BAD_REQUEST, msg),
-            AppError::IoError(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
-            AppError::NotFound(msg) => (StatusCode::NOT_FOUND, msg),
-            AppError::InternalServerError(msg) => (StatusCode::INTERNAL_SERVER_ERROR, msg),
-        };
-
-        let body = Json(json!({
-            "error": {
-                "message": error_message,
-                "code": status.as_u16()
-            }
-        }));
-
-        (status, body).into_response()
-    }
 }
 
 /// Application result type
